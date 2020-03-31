bd92ae3ffda5c7794ac35dfff6e9a2549ee287d0
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-03-31 14:58:50 +0200

    Fixed @include directive and double slash in URL

--- AliClean.user.js
+++ AliClean.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name        AliClean
-// @version     1.0
 // @description Clean aliexpress item URL
 // @license     BSD 3-Clause
 // @author      Duckle29
 // @namespace   https://github.com/Duckle29
-// @include     /^https?://(www\.)?aliexpress\.com/((item|store/product)/.*/
+// @include     /^https?:\/\/(www\.)?aliexpress\.com\/(item|store\/product)\/.*/
 // @run-at      document-start
 // @connect     githubusercontent.com
 // @connect     raw.githubusercontent.com
@@ -13,12 +12,14 @@
 // @homepageURL https://github.com/Duckle29/AliClean
 // @downloadURL https://raw.githubusercontent.com/Duckle29/AliClean/master/AliClean.user.js
 // @updateURL   https://raw.githubusercontent.com/Duckle29/AliClean/master/AliClean.user.js
+// @version     1.1
+// @history     1.1 Fixed @include directive and double slash in URL
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var p1 = document.URL.match(/https?:\/\/(www\.)?aliexpress.com\/(?:item|store\/product)\//g);
+    var p1 = document.URL.match(/^https?:\/\/(www\.)?aliexpress.com\/(?:item|store\/product)/g);
     var p2 = document.URL.match(/\/[0-9_]+[.]html(?=$|[?])/g);
     if (p1 && p2 && p1+p2 != document.URL) history.replaceState(null, '', p1+p2);
 })();
